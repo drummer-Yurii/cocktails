6838ae972332d54fac3cef5b69695778a6918fda
Yurii Rybachok <drummer2009@ukr.net>
2023-05-12 13:51:34 +0300

    create select for choise ingredients

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,14 @@
 <script setup>
+import { ref } from 'vue';
 import AppLayout from "../components/AppLayout.vue";
 import { useRootStore } from "../stores/root";
+import { storeToRefs } from 'pinia';
 
 const rootStore = useRootStore()
 rootStore.getIngredients()
+
+const { ingredients } = storeToRefs(rootStore);
+const ingredient = ref(null);
 </script>
 
 <template>
@@ -12,6 +17,21 @@ rootStore.getIngredients()
             <div class="info">
                 <div class="title">Choose your drink</div>
                 <div class="line"></div>
+                <div class="select-wrapper">
+                    <el-select 
+                        v-model="ingredient" 
+                        placeholder="Choose main ingredient" 
+                        size="large"
+                        class="select"
+                    >
+                        <el-option
+                        v-for="item in ingredients"
+                        :key="item.strIngredient1"
+                        :label="item.strIngredient1"
+                        :value="item.strIngredient1"
+                        />
+                    </el-select>
+                </div>
             </div>
         </div>
     </AppLayout>
@@ -28,4 +48,10 @@ rootStore.getIngredients()
 .info
     padding: 80px 0
     text-align: center
+
+.select-wrapper
+    padding-top: 50px
+
+.select
+    width: 220px
 </style>
