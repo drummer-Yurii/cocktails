b0d8ef59672b3a56ad8c40c8c3612c1ec6ce6141
Yurii Rybachok <drummer2009@ukr.net>
2023-06-08 20:19:53 +0300

    create slider for cocktail random

--- src/pages/CocktailRandom.vue
+++ src/pages/CocktailRandom.vue
@@ -2,23 +2,23 @@
 import axios from 'axios';
 import { ref, computed } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
-import { COCKTAIL_RANDOM } from '@/constants';
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+import { COCKTAIL_RANDOM, INGREDIENT_PIC } from '@/constants';
 import AppLayout from "../components/AppLayout.vue";
 
 const route = useRoute();
 const router = useRouter();
 
 const cocktail = ref(null);
-const cocktailId = computed(() => route.path.split('/').pop());
 
 const ingredients = computed(() => {
     const ingredients = [];
 
     for (let i = 1; i <= 15; i++) {
         if (!cocktail.value[`strIngredient${i}`]) break;
-        const ingredient = {};
-        ingredient.name = cocktail.value[`strIngredient${i}`];
-        ingredient.measure = cocktail.value[`strMeasure${i}`];
+
+        const ingredient = cocktail.value[`strIngredient${i}`];
 
         ingredients.push(ingredient);
     }
@@ -45,6 +45,25 @@ getCocktail();
                 <div class="info">
                     <div class="title">{{ cocktail.strDrink }}</div>
                     <div class="line"></div>
+
+                    <div class="slider">
+                        <swiper
+                            :slides-per-view="3"
+                            :space-between="50"
+                            class="swiper"
+                        >
+                            <swiper-slide
+                                v-for="(ingredient, key) in ingredients"
+                                :key="key"
+                            >
+                                <img :src="`${INGREDIENT_PIC}${ingredient}-Small.png`" />
+                                <div class="name">
+                                    {{ ingredient }}
+                                </div>
+                            </swiper-slide>
+                        </swiper>
+                    </div>
+
                     <div class="instructions">
                         {{ cocktail.strInstructions }}
                     </div>
@@ -56,4 +75,13 @@ getCocktail();
 
 <style lang="sass" scoped>
 @import '../assets/styles/main'
+
+.slider 
+    padding: 50px 0
+
+.swiper
+    width: 586px 
+
+.name
+    padding-top: 20px   
 </style>
